1d1efa51b8433215849c1f9db0f1a25845430d4f
Liya P. <alisapuhtel@gmail.com>
2025-10-16 01:02:16 +0300

    Добавлены тесты Selenide и обновлен build.gradle

---
+++ src/test/java/GitHubTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GitHubTest {
+    
+    @Test
+    void simpleGitHubTest() {
+        // Шаг 4: Открываем страницу GitHub
+        open("https://github.com/Vasilissa-cod/at-java-study");
+        
+        // Шаг 5: Проверяем наличие текста "gradle/wrapper"
+        String pageText = $("body").getText();
+        assertTrue(pageText.contains("gradle/wrapper"));
+        
+        // Шаг 7: Проверяем наличие текста "src/test/java" (это должно вызвать ошибку)
+        $("body").shouldHave(text("src/test/java"));
+    }
+}
