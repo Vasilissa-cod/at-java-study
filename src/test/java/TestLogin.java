508af5f6b5f02ca57e45cd6819f598945226d4dd
Liya P. <alisapuhtel@gmail.com>
2025-10-23 20:27:50 +0300

    Add TestLogin and LocatorsTests classes with Selenide automation tests

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,14 @@
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+public class TestLogin {
+
+    @Test
+    void test01_find_element() {
+        open("https://slqa.ru/cases/ChatGPTLogin/");
+        $(By.id("username")).setValue("standard_user");
+        sleep(5_000);
+    }
+}
